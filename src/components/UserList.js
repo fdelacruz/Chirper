70ebdbe22b1478eaabd692cf51bb47d015934984
Francisco De La Cruz <helloquico@gmail.com>
2015-12-19 12:46:42 -0500

    Create the User List Component

---
+++ src/components/UserList.js
@@ -0,0 +1,35 @@
+var React = require('react');
+var UserStore = require('../stores/users');
+var actions = require('../actions');
+var Link = require('react-router').Link;
+
+var Box = require('./ChirpBox');
+
+var UserList = module.exports = React.createClass({
+	getInitialState: function () {
+		return {
+			users: UserStore.all(),
+			user: UserStore.currentUser
+		};
+	},
+	componentDidMount: function () {
+		UserStore.addChangeListener(this.onChange);
+	},
+	componentWillUnmount: function () {
+		UserStore.removeChangeListener(this.onChange);
+	},
+	onChange: function () {
+		this.setState(this.getInitialState());
+	},
+	render: function () {
+		var items = this.state.users.filter(function (user) {
+			return this.state.user.cid !== user.cid;
+		}.bind(this)).map(function (user){
+			return <Box user={user} key={user.cid}>
+						Follow Button
+				</Box>;
+		});
+
+		return <ul> {items} </ul>;
+	}
+});
